6665a716c01aed731b0e2bf70f1bb7796681c095
dekkowka <119127047+dekkowka@users.noreply.github.com>
2022-11-27 23:03:54 +0500

    Add files via upload

---
+++ src/task12.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class task12 {
+    public static void main(String[] args) {
+        System.out.println("Введите число");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int numberMinusOne = number - 1;
+        int numberPlusOne = number + 1;
+        int sum = number + numberMinusOne + numberPlusOne;
+        int numberSquared = (int) Math.pow(sum, 2);
+        System.out.println(numberMinusOne + " " + number + " " + numberPlusOne + " " + numberSquared);
+    }
+}
